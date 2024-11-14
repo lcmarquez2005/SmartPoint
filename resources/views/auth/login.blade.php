042f4bb97453ebc47dd376ff1cea94b3726f30f6
lcmarquez2005 <93497992+lcmarquez2005@users.noreply.github.com>
2024-11-14 11:52:18 -0600

    modificacion del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,12 +14,15 @@
 
                         <div class="form-group">
                             <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username: (Unico)</label>
-                            <input id="username" type="text" name="username" required autofocus class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <input id="username" type="text" name="username" required autofocus class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                         </div>
 
-                        <div class="form-group">
-                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
-                            <input id="password" type="password" name="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                        <div class="form-group row">
+                            <label for="password" class="col-sm-2 col-form-label">Password:</label>
+                            <div class="col-sm-10">
+
+                                <input id="password" type="password" name="password" required class="form-control col-sm-8">
+                            </div>
                         </div>
 
                         <div class="form-group">
